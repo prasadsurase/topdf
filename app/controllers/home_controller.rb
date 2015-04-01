4b6fc6bb6add7c536258d43963200698127e7605
Prasad Surase <prasadsurase@gmail.com>
2015-04-01 13:56:40 +0530

    Added settings for pdfkit.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,9 @@ class HomeController < ApplicationController
       format.html
       format.pdf do
         html = render_to_string(layout: true , action: "generate_pdf.html.haml")
-        kit = PDFKit.new(html, page_size: 'A4', print_media_type: true)
+        kit = PDFKit.new(html, page_size: 'A4', print_media_type: true, orientation: 'Landscape' )
+        kit.stylesheets << Rails.application.assets['application.css'].pathname.to_s
+binding.pry
         pdf = kit.to_pdf
         send_data pdf, filename: 'booklet.pdf', type: 'application/pdf'
       end
--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,4 +1,11 @@
 PDFKit.configure do |config|
   config.default_options[:quiet] = false
   config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'
+  config.default_options = {
+    :page_size     => 'Letter',
+    :margin_top    => '0.5in',
+    :margin_right  => '0.25in',
+    :margin_bottom => '0.5in',
+    :margin_left   => '0.25in'
+  }
 end
