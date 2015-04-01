99da880e6084f77a8cc77af5e954e0743c7841c5
Prasad Surase <prasadsurase@gmail.com>
2015-04-01 15:07:12 +0530

    Added hack to load the css.scss files in pdfkit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,8 +9,9 @@ class HomeController < ApplicationController
       format.pdf do
         html = render_to_string(layout: true , action: "generate_pdf.html.haml")
         kit = PDFKit.new(html, page_size: 'A4', print_media_type: true, orientation: 'Landscape' )
-        kit.stylesheets << Rails.application.assets['application.css'].pathname.to_s
-binding.pry
+        `sass app/assets/stylesheets/application.css.scss tmp/app.css`
+        #kit.stylesheets << Rails.application.assets['application.css'].pathname.to_s
+        kit.stylesheets << "#{Rails.root}/tmp/app.css"
         pdf = kit.to_pdf
         send_data pdf, filename: 'booklet.pdf', type: 'application/pdf'
       end
