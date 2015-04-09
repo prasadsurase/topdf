203af89b372326ae289a13ecd4963a94f2015eff
Prasad Surase <surase_prasad@yahoo.co.in>
2015-04-09 15:25:07 +0530

    Added images refered in css and new initializer images hash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,7 @@ class HomeController < ApplicationController
       format.pdf do
         html = render_to_string(layout: true , action: "generate_pdf.html.haml")
         kit = PDFKit.new(html, page_size: 'A4', orientation: 'Landscape' )
-        `sass vendor/assets/stylesheets/bootstrap.scss tmp/bootstrap.css`
-        `sass vendor/assets/stylesheets/custom.scss tmp/custom.css`
-        kit.stylesheets << "#{Rails.root}/tmp/bootstrap.css"
-        kit.stylesheets << "#{Rails.root}/tmp/custom.css"
+        kit.stylesheets << "#{Rails.root}/app/assets/stylesheets/pdf.css"
         pdf = kit.to_pdf
         send_data pdf, filename: 'booklet.pdf', type: 'application/pdf'
       end
