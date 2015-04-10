d285d381f6eb0e7971b5a289ee14e74cca5cd9c2
Prasad Surase <prasadsurase@gmail.com>
2015-04-10 18:10:28 +0530

    Added stylesheets and some more text to html page

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A sample repository to check as to how to load images from Facebook CDN in pdf u
 
 Do not use wkhtmltopdf-binary gem. Instead install it using 'sudo apt-get install wkhtmltopdf'. Create a file 'config/initializers/pdfkit.rb' and add
 "config.wkhtmltopdf = '/usr/bin/wkhtmltopdf'" to the "PDFKit.configure" block.
+
+Clone at '~/', bundle and run.
--- config/initializers/images.rb
+++ config/initializers/images.rb
@@ -1,11 +1,5 @@
 IMAGES = [
   {
-    height: 720,
-    title: nil,
-    url: "https://scontent.xx.fbcdn.net/hphotos-xap1/t31.0-8/s720x720/980014_10201388496663031_970930963_o.jpg",
-    width: 540
-  },
-  {
     height: 684,
     title: nil,
     url: "https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/941632_10201329316423562_732613430_n.jpg?oh=cbefc7b72200918f73232539679f6b27&oe=55AD7FD8&__gda__=1437599985_4d130153e012cb330e8e828d5b31d82b",
@@ -27,12 +21,6 @@ IMAGES = [
     height: 432,
     title: nil,
     url: "https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xap1/t31.0-8/s720x720/906468_10201023370815113_668460846_o.jpg",
-    width: 720
-  },
-  {
-    height: 540,
-    like_count: 8,
-    url: "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/v/t35.0-12/p180x540/992095_10201394798860582_1046330881_o.jpg?oh=107260a110ecfceaecfbed3aaab37aba&oe=552818E5&__gda__=1428629491_77f8bed0655c3129da52f6bc5dfa19a4",
     width: 720
   },
   {
